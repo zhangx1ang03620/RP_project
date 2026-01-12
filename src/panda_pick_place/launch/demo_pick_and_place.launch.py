f881b554885eea4375e1020aeba5856b7e8ad83a
zhangx1ang03620 <zhangx1ang03620@gmail.com>
2026-01-12 17:06:11 +0800

    Update demo_pick_and_place.launch.py

--- src/panda_pick_place/launch/demo_pick_and_place.launch.py
+++ src/panda_pick_place/launch/demo_pick_and_place.launch.py
@@ -2,9 +2,8 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from moveit_configs_utils import MoveItConfigsBuilder
 
-
 def generate_launch_description():
-    # 正确的 MoveIt 配置加载
+    # MoveIt 配置加载
     moveit_config = (
         MoveItConfigsBuilder("panda", package_name="moveit_resources_panda_moveit_config")
         .to_dict()
@@ -12,8 +11,8 @@ def generate_launch_description():
 
     # 启动 MTC Demo 节点
     pick_place_demo = Node(
-        package="panda_pick_place",
-        executable="mtc_tutorial",
+        package="panda_pick_place",    
+        executable="pick_place_node",  
         output="screen",
         parameters=[moveit_config],
     )
